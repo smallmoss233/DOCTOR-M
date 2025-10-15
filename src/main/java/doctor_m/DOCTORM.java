9df1b4acb41d0f270a5ed1a55ef771d0467921c8
Small Moss <2304178911@qq.com>
2025-10-15 12:09:17 +0800

    注释掉有问题的代码...

--- src/main/java/doctor_m/DOCTORM.java
+++ src/main/java/doctor_m/DOCTORM.java
@@ -16,11 +16,11 @@ public class DOCTORM implements ModInitializer {
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    public static final Block one_BLOCK = new Block(FabricBlockSettings.create().strength(4.0f));
+    //public static final Block one_BLOCK = new Block(FabricBlockSettings.create().strength(4.0f));
 
 	@Override
 	public void onInitialize() {
-        Registry.register(Registries.BLOCK,new Identifier("doctor_m", "one_block"),one_BLOCK);
+        //Registry.register(Registries.BLOCK,new Identifier("doctor_m", "one_block"),one_BLOCK);
 
 		LOGGER.info("Doctor M Mod 已加载完成！");
 	}
