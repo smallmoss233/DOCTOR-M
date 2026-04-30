40cdb51ab7120d4015e34b7bb1c5192664760eee
Small Moss <2304178911@qq.com>
2026-04-30 17:00:23 +0800

    使用amblekit大幅简化物品的注册代码，同时加入了台湾和香港语言的翻译

--- src/main/java/doctor_m/Item/itmes_weapon.java
+++ src/main/java/doctor_m/Item/itmes_weapon.java
@@ -1,27 +1,18 @@
 package doctor_m.Item;
 
+import dev.amble.lib.container.impl.ItemContainer;
 import doctor_m.Item.data_weapon.de_mat_gun;
 import doctor_m.Item.data_weapon.rassilon_key;
 import net.minecraft.item.Item;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
 
-public class itmes_weapon {
+public class itmes_weapon extends ItemContainer {
+
     public static final Item DE_MAT_GUN = new de_mat_gun(new Item.Settings()
             .maxCount(1)
             .rarity(Rarity.EPIC));
+
     public static final Item RASSILON_KEY = new rassilon_key(new Item.Settings()
             .maxCount(1)
             .rarity(Rarity.EPIC));
-
-    public static void registerItems() {
-        Registry.register(Registries.ITEM, id("de_mat_gun"), DE_MAT_GUN);
-        Registry.register(Registries.ITEM, id("rassilon_key"), RASSILON_KEY);
-    }
-
-    private static Identifier id(String path) {
-        return new Identifier("doctor_m", path);
-    }
 }
