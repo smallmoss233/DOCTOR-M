dfb284074e47ed84606953a05d848d3dec9e4b6c
Small Moss <2304178911@qq.com>
2026-06-14 09:11:30 +0800

    修复反射工具类的BUG

--- src/main/java/doctor_m/util/ShiftTooltipInvoker.java
+++ src/main/java/doctor_m/util/ShiftTooltipInvoker.java
@@ -1,5 +1,7 @@
 package doctor_m.util;
 
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.api.EnvType;
 import net.minecraft.text.Text;
 import java.lang.reflect.Method;
 import java.util.List;
@@ -8,14 +10,17 @@ public class ShiftTooltipInvoker {
     private static final String HELPER_CLASS = "doctor_m.util.ShiftTooltipHelper";
     private static final String METHOD_NAME = "addShiftTooltip";
 
-    public static void addShiftTooltip(List<Text> tooltip, Text shortText, Text... detailedLines) {
+    public static void addShiftTooltip(List<Text> tooltip, Text longText) {
+        if (FabricLoader.getInstance().getEnvironmentType() != EnvType.CLIENT) {
+            return;
+        }
+
         try {
             Class<?> clazz = Class.forName(HELPER_CLASS);
-            Method method = clazz.getDeclaredMethod(METHOD_NAME, List.class, Text.class, Text[].class);
-            method.invoke(null, tooltip, shortText, detailedLines);
+            Method method = clazz.getDeclaredMethod(METHOD_NAME, List.class, Text.class);
+            method.invoke(null, tooltip, longText);
         } catch (Exception e) {
-            // 回退：仅显示简短描述
-            tooltip.add(shortText);
+            e.printStackTrace(); // 记录错误日志
         }
     }
 }
