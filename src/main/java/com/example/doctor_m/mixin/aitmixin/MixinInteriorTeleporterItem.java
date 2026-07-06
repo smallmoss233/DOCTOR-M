05480ffe5d771b8ed42082c8ce49c320b8215a10
Small Moss <2304178911@qq.com>
2026-07-06 10:23:11 +0800

    确定版本号变更为2.1 AIT的哈赞卓水晶不再是消耗物品本身，而是消耗物品耐久（16）

---
+++ src/main/java/com/example/doctor_m/mixin/aitmixin/MixinInteriorTeleporterItem.java
@@ -0,0 +1,32 @@
+package com.example.doctor_m.mixin.aitmixin;
+
+import dev.amble.ait.core.item.InteriorTeleporterItem;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(InteriorTeleporterItem.class)
+public class MixinInteriorTeleporterItem {
+
+    @Redirect(
+            method = "use",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/item/ItemStack;setCount(I)V"
+            )
+    )
+    private void replaceConsumptionWithDurability(
+            ItemStack stack, int count,
+            World world, PlayerEntity user, Hand hand
+    ) {
+        if (count == stack.getCount() - 1) {
+            stack.damage(1, user, p -> {});
+        } else {
+            stack.setCount(count);
+        }
+    }
+}
