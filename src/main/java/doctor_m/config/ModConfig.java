b3f223dbebccf726c9f45f9ee5a798a63db0d960
Small Moss <2304178911@qq.com>
2026-09-16 17:51:49 +0800

    VM冷却时间降低到10s

--- src/main/java/doctor_m/config/ModConfig.java
+++ src/main/java/doctor_m/config/ModConfig.java
@@ -134,7 +134,7 @@ public class ModConfig {
     //涡旋操纵器
     public int vortexManipulatorMaxFuel = 1500;                // 最大燃料
     public int vortexManipulatorMaxOverheat = 100;             // 最大过热
-    public int vortexManipulatorCooldownTicks = 1200;          // 普通冷却（tick，1200=60秒）
+    public int vortexManipulatorCooldownTicks = 200;          // 普通冷却（tick，200=10秒）
     public long vortexManipulatorBrokenCooldownTicks = 72000L; // 损坏恢复时间（3游戏日）
     public int vortexManipulatorCoolingIntervalTicks = 80;     // 散热间隔（tick）
     public int vortexManipulatorCoolingPerInterval = 1;        // 每次散热减少的过热量
